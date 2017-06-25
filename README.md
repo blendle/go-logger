a8cbb5535584af7b58e53313a22cf5e8b2ec14f4
Jean Mertz <jean@mertz.fm>
2017-06-25 22:55:17 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# go-logger
+
+This package contains shared logic for Go-based logging.
+
+This package uses the [`zap`][zap] logging package under the hood. See its
+[README][zap] for more details on the logging API.
+
+See [this wiki page][wiki] for more details on logging at Blendle.
+
+[zap]: https://github.com/uber-go/zap#zap-zap---
+[wiki]: https://www.notion.so/blendle/Structured-Logging-5e6fb67cd17c42acb7f180c37f436c2a
+
+## Usage
+
+Add to your `main.go`:
+
+```golang
+import logger "github.com/blendle/go-logger"
+
+func init() {
+	c := &logger.Config{
+		App:         "my-app",
+		Tier:        "api",
+		Production:  false,
+		Version:     "cf89f839",
+		Environment: "staging",
+	}
+
+	logger.Init(c)
+}
+```
+
+Then use it throughout your application:
+
+```golang
+logger.L.Warn("Something happened!")
+```
+
+### Debugging
+
+You can send the `usr1` signal to your application to switch the log level
+between the default `INFO` and `DEBUG` level on runtime.
+
+This allows you to capture debug logs during anomalies and find the problem.
+
+You can also set the `DEBUG` environment variable to `true` to have the
+application launch with the default log level set to `DEBUG` instead of `INFO`.
+
+Again, you can send `usr1` to toggle back to `INFO` as well.
---
+++ logger.go
@@ -0,0 +1,91 @@
+package logger
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Config of the current logger instance
+type Config struct {
+	App         string
+	Tier        string
+	Production  bool
+	Version     string
+	Environment string
+}
+
+// L is the global logger instance.
+var L *zap.Logger
+
+// LogLevel is the current log level of the logger.
+var LogLevel = zap.NewAtomicLevel()
+
+// Init initializes the logger
+func Init(config *Config) {
+	var err error
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "severity",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	}
+
+	zapconfig := zap.Config{
+		Level:       LogLevel,
+		Development: !config.Production,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:          "json",
+		EncoderConfig:     encoderConfig,
+		OutputPaths:       []string{"stderr"},
+		ErrorOutputPaths:  []string{"stderr"},
+		DisableCaller:     true,
+		DisableStacktrace: true,
+	}
+
+	if os.Getenv("DEBUG") == "true" {
+		zapconfig.Level.SetLevel(zap.DebugLevel)
+	}
+
+	go logLevelToggler()
+
+	app := zap.String("app", config.App)
+	tier := zap.String("tier", config.Tier)
+	production := zap.Bool("production", config.Production)
+	version := zap.String("version", config.Version)
+	environment := zap.String("environment", config.Environment)
+
+	L, err = zapconfig.Build(zap.Fields(app, tier, production, version, environment))
+	if err != nil {
+		log.Printf(`{"severity": "fatal", "msg": "%v"}`, err)
+		os.Exit(1)
+	}
+}
+
+func logLevelToggler() {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGUSR1)
+
+	for {
+		<-ch
+
+		if LogLevel.Level() == zap.DebugLevel {
+			LogLevel.SetLevel(zap.InfoLevel)
+		} else {
+			LogLevel.SetLevel(zap.DebugLevel)
+		}
+	}
+}
