ff6288c9e93be777650edc2c8dace40f15a6b549
Jean Mertz <jean@mertz.fm>
2018-05-21 12:50:47 +0200

    Initialize logger by extending existing logger

--- logger.go
+++ logger.go
@@ -5,14 +5,8 @@ import (
 )
 
 // New returns a new logger, ready to use in our services.
-func New(service, version string, options ...func(zap.Config)) *zap.Logger {
-	config := zap.NewProductionConfig()
+func New(service, version string, zaplog *zap.Logger) *zap.Logger {
 	fields := zap.Fields(zap.String("service", service), zap.String("version", version))
 
-	logger, err := config.Build(fields)
-	if err != nil {
-		panic(err)
-	}
-
-	return logger
+	return zaplog.WithOptions(fields)
 }
--- logger_test.go
+++ logger_test.go
@@ -5,11 +5,15 @@ import (
 
 	logger "github.com/blendle/go-logger"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
 
 func TestNew(t *testing.T) {
 	t.Parallel()
 
-	assert.IsType(t, &zap.Logger{}, logger.New("", ""))
+	zaplog, err := zap.NewProduction()
+	require.NoError(t, err)
+
+	assert.IsType(t, &zap.Logger{}, logger.New("", "", zaplog))
 }
